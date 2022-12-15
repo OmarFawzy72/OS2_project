503d4496c6b597f65df9bc057051731551423c06
shehabziada130 <84864669+shehabziada130@users.noreply.github.com>
2022-12-15 17:23:07 +0200

    Add files via upload

---
+++ Dining_Philosophers/DiningTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dining_Philosophers;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pouya
+ */
+
+public class DiningTest {
+         static int K;
+    public static void main(String[] args){
+        System.out.println("Enter number of Philosophers : ");
+        Scanner scan = new Scanner(System.in);
+        int n=scan.nextInt();
+        K=n;
+        
+        Chopstick CH = new Chopstick();
+        for(int i =0; i<K;i++)
+        {
+            new Philosopher(CH).start();
+        }
+    }
+    }
+
---
+++ Dining_Philosophers/Philosopher.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dining_Philosophers;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pouya
+ */
+class Philosopher extends Thread{
+    int Num;
+    static int Number=0;
+    private Chopstick Chop;
+    public Philosopher(Chopstick Chop){
+	super();
+	this.Chop=Chop;
+        Num=Number;
+	Number++;
+    }
+
+private void hungry(){
+        System.out.format("Philosopher\t%d\tis Hungry\n", Num);
+                };    
+    private void eating(){
+        System.out.format("Philosopher\t%d\tis Eating\n", Num);
+        try {Thread.sleep(500);
+        } catch (InterruptedException e) {}
+    }
+    private void thinking(){
+        System.out.format("Philosopher\t%d\tis Thinking\n", Num);
+        try { Thread.sleep(500);
+        } catch (InterruptedException e) {}
+    }
+    public void run(){
+        while(true){
+            thinking();
+            hungry();
+            Chop.take();
+            eating();
+            Chop.release();
+        }
+    }
+}
+
+
+
